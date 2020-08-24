118239d94355adb2a32075dd018c6eda1ca7674b
Alex Buznik <shu@buznik.net>
2020-08-24 16:57:44 +0300

    Add AC update

--- server/index.php
+++ server/index.php
@@ -1,7 +1,7 @@
 <?php
 /* Air Conditioning settings */
 define(API_VERSION, 1);
-
+define(CONFIG_FILE, './config');
 define(DELIMITER, ';');
 
 define(UNIT_MODES, ['cold' => 1, 'hot' => 2, 'dry' => 3, 'vent' => 4]);
@@ -18,7 +18,7 @@ $unit_template = array(
 
 $unit = '';
 $state = null;
-$config_value = file_get_contents('./config');
+$config_value = file_get_contents(CONFIG_FILE);
 if (strlen($config_value) > 0) {
   $state = json_decode($config_value, TRUE);
 }
@@ -49,6 +49,37 @@ function filter_units($state_, $unit_) {
   return $found;
 }
 
+function valid_ac_values($item) {
+  global $unit;
+  if ($item == $unit) {
+    return $item;
+  }
+  return intval($item);
+}
+
+function update_unit($state_, $unit_, $updated) {
+  $found = filter_units($state_, $unit_);
+  if (is_null($found)) {
+    return header("HTTP/1.1 404 Not Found");
+  }
+  $valid_keys = array_intersect_key($updated, $found);
+  $valid_values = array_map('valid_ac_values', $valid_keys);
+
+  for($i = 0; $i < count($state_['units']); $i++) {
+    if ($state_['units'][$i]['name'] == $unit_) {
+      // found, replacing
+      $state_['units'][$i] = $valid_values;
+    }
+  }
+  save_state($state_);
+  return true;
+}
+
+function save_state($state_) {
+  $state_json = json_encode($state_);
+  file_put_contents(CONFIG_FILE, $state_json);
+}
+
 // getting config
 $method = $_SERVER['REQUEST_METHOD'];
 
@@ -60,7 +91,17 @@ if ($method == 'GET') {
     echo json_encode($state);
   }
 } else if ($method == 'POST') {
-  echo "Not implemented";
+  $unit = substr($_POST['name'], 0, 16);
+  if (strlen($unit) > 0) {
+    $update = update_unit($state, $unit, $_POST);
+    if ($update == true) {
+      return header("HTTP/1.1 200 OK");
+    } else {
+      return header("HTTP/1.1 500 Server Error");
+    }
+  } else {
+    return header('HTTP/1.1 400 Bad Request');
+  }
 }
 
 ?>
